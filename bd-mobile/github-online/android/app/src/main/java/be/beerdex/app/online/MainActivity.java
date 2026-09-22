471d51ca037c88f41451d936047a093b6d441c6b
Noah Bruijninckx <66059559+DualsFWShield@users.noreply.github.com>
2026-09-22 11:00:09 +0200

    feat: add custom Android app themes and back button handling for WebViews

--- bd-mobile/github-online/android/app/src/main/java/be/beerdex/app/online/MainActivity.java
+++ bd-mobile/github-online/android/app/src/main/java/be/beerdex/app/online/MainActivity.java
@@ -1,17 +1,9 @@
 package be.beerdex.app.online;
 
-import android.os.Bundle;
 import android.webkit.WebView;
-import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public void onBackPressed() {
--- bd-mobile/play-store-online/android/app/src/main/java/co/median/android/yeenpjz/MainActivity.java
+++ bd-mobile/play-store-online/android/app/src/main/java/co/median/android/yeenpjz/MainActivity.java
@@ -1,17 +1,9 @@
 package co.median.android.yeenpjz;
 
-import android.os.Bundle;
 import android.webkit.WebView;
-import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public void onBackPressed() {
